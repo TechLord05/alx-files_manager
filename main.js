b9590f8760d77933eae61fa0605734d27ab106b3
Pcosby5 <pcosby50@gmail.com>
2024-06-30 12:59:38 +0200

    db.js

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-import redisClient from './utils/redis';  // Ensure correct relative path to redis.js
+import dbClient from './utils/db';
+
+const waitConnection = () => new Promise((resolve, reject) => {
+  let i = 0;
+  const repeatFct = async () => {
+    setTimeout(async () => {
+      i += 1;
+      if (i >= 10) {
+        reject(new Error('Connection timeout'));
+      } else if (!dbClient.isAlive()) {
+        repeatFct();
+      } else {
+        resolve();
+      }
+    }, 1000);
+  };
+  repeatFct();
+});
 
 (async () => {
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
-
-    setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000*10)
+  console.log(dbClient.isAlive());
+  try {
+    await waitConnection();
+    console.log(dbClient.isAlive());
+    console.log(await dbClient.nbUsers());
+    console.log(await dbClient.nbFiles());
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
 })();
