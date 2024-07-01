3ce8d4f55ba331f1edaf20e6907d772d1f2a8a3b
Pcosby5 <pcosby50@gmail.com>
2024-07-01 17:32:04 +0000

    created user

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,19 @@ import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
+// Route to get the status of Redis and MongoDB clients
 router.get('/status', AppController.getStatus);
+
+// Route to get the statistics of the application
 router.get('/stats', AppController.getStat);
 
+// Route to create a new user
+router.post('/users', AppController.createUser);
+
+// Route to find a user by email
+router.get('/users', AppController.findUser);
+
 export default (app) => {
+  // Use the defined routes in the express application
   app.use('/', router);
 };
